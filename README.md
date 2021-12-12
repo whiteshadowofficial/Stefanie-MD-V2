0bba578f444793fe02b077ed3b4abe52aede158d
KAWEESHA CHAMOD <88616416+Kaweeshachamodk@users.noreply.github.com>
2021-12-12 12:19:12 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,46 @@
+## WhatsApp User Bot
+
+- This is a Userbot based on 2 repositories written by [Kaweesha Chamod](https://github.com/)
+
+## Besed on
+1. Click to whatch [Npm For Bot](https://github.com/Kaweeshachamodk/npm) and see futures in bot.
+2. Click to whatch [Data Base For Bot](https://github.com/Kaweeshachamodk/DataBase) and see Data protection in bot.
+
+## Setup
+
+1. Click [SCAN](https://replit.com/@Kaweeshachamodk/STEFANIE-BETA-NEW-6?v=1) and scan QR through Whatsapp Linked Devices Option in Your whatsapp App.
+2. You will get a long text that needs after 4th step (STEFANIE_SESSION).
+3. If You don't have a account in [Heroku](https://signup.heroku.com/), Create a account.
+4. Now click [DEPLOY](https://heroku.com/deploy?template=https://github.com/Kaweeshachamodk/WhatApp-Bot) and Follow.
+   <br>
+   <a href="https://youtu.be/GNSARiAUfKA"><img src="https://img.shields.io/badge/-watch%20video-critical?style=for-the-badge&logo=youtube&logoColor=white">
+   <br>
+## Special Futures in this bot
+
+- Button menu
+- 2 step alive message (pp and link)
+- Group crashing system 
+- Bot bug system 
+- V card genareting option
+
+
+## Note this
+- Exclusively from VIRUS FUCKER Project 
+- Do not use this fore any commercial thing
+- Do not edit (Respect to the Devaoloper) 
+- All rights reserved VIRUS FUCKER
+
+
+## Thanks To
+- [@adiwajshing](https://github.com/adiwajshing) for coded [Baileys](https://github.com/adiwajshing/Baileys) 
+- [@Himsham-Mohammed](https://github.com/Hisham-Muhammed) special thanks for help & advancing
+- [@Prabath Senavirathna](https://github.com/MrChaby) for this coded
+- [@Prabath Senavirathna](https://github.com/MrChaby) for he give me special help to create my bot.
+- Translators
+
+## License
+This project is protected by `GNU General Public Licence v3.0` license.
+Assign Credit to developers.Dont edit out the copyright messages!
+
+### Disclaimer
+`WhatsApp` name, its variations and the logo are registered trademarks of Facebook. We have nothing to do with the registered trademark
---
+++ plugins/ezysetvar.js
@@ -0,0 +1,55 @@
+/* Copyright (C) 2021 Hyper sir
+coded for Fidha mwol
+*/
+
+const Julie = require('../events');
+const config = require('../config');
+const Heroku = require('heroku-client');
+const heroku = new Heroku({
+    token: config.HEROKU.API_KEY
+});
+let baseURI = '/apps/' + config.HEROKU.APP_NAME;
+
+    Julie.addCommand({ pattern: 'sudo ?(.*)', fromMe: true, desc: 'changes sudo numbers', usage: '.sudo *your number*' }, (async (message, match) => {
+        if (match[1] == '') return await message.sendMessage('ɴᴇᴇᴅ ᴀ ɴᴜᴍʙᴇʀ')
+        await heroku.patch(baseURI + '/config-vars', {
+            body: {
+                ['SUDO']: match[1]
+            }
+        });
+        await message.sendMessage("ɴᴇᴡ sᴜᴅᴏ ᴜᴘᴅᴀᴛᴇᴅ")
+    }));
+
+    Julie.addCommand({ pattern: 'caption ?(.*)', fromMe: true, desc: 'changes all captions', usage: '.caption Made by FidhaMwol' }, (async (message, match) => {
+        if (match[1] == '') return await message.sendMessage('ɴᴇᴇᴅ ᴀ ᴄᴀᴘᴛɪᴏɴ ')
+        await heroku.patch(baseURI + '/config-vars', {
+            body: {
+                ['ALL_CAPTION']: match[1]
+            }
+        });
+        await message.sendMessage("ɴᴇᴡ ᴄᴀᴘᴛɪᴏɴ ᴜᴘᴅᴀᴛᴇᴅ ")
+    }));
+
+    Julie.addCommand({ pattern: 'handlers ?(.*)', fromMe: true, desc: 'changes handlers', usage: '.handler ^[.!] ' }, (async (message, match) => {
+        if (match[1] == '') return await message.sendMessage('ɴᴇᴇᴅ ᴀ ʜᴀɴᴅʟᴇʀ')
+        await heroku.patch(baseURI + '/config-vars', {
+            body: {
+                ['ALL_CAPTION']: match[1]
+            }
+        });
+        await message.sendMessage("ɴᴇᴡ ʜᴀɴᴅʟᴇʀ ᴜᴘᴅᴀᴛᴇᴅ ")
+    }));
+
+
+    Julie.addCommand({ pattern: 'botname ?(.*)', fromMe: true, desc: 'change your bot name', usage: '.botname name ' }, (async (message, match) => {
+        if (match[1] == '') return await message.sendMessage('ᴛʏᴘᴇ ʏᴏᴜʀ ɴᴇᴡ ʙᴏᴛ ɴᴀᴍᴇ')
+        await heroku.patch(baseURI + '/config-vars', {
+            body: {
+                ['BOT_NAME']: match[1]
+            }
+        });
+        await message.sendMessage("ɴᴇᴡ ʙᴏᴛ ɴᴀᴍᴇ ᴜᴘᴅᴀᴛᴇᴅ ")
+    }));
+
+
+
